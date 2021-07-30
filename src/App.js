20cf4b574d6f85a661f63ecc6bc449b5b8f2f8f8
Lutfullo <munavarovlutfullo3@gmail.com>
2021-07-30 17:21:39 +0500

    add new changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Start from "./components/Onload/start";
 import Header from "./components/Header/header";
 import Bomdod from "./components/Namoz/Bomdod/bomdod";
